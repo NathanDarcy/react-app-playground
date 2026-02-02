6e7f548e8ecad6215b5e64d20ba1f16201ef192e
Nathan Darcy <nathandarcy33@gmail.com>
2026-02-02 13:10:23 -0600

    implement main header - Nathan

--- src/features/grandHotel/Dashboard.tsx
+++ src/features/grandHotel/Dashboard.tsx
@@ -2,6 +2,7 @@ import './grand-hotel.css'
 import Sidebar from './components/Sidebar'
 import Navigation from './components/Navigation'
 import { useState } from 'react'
+import crown from './images/crown.png'
 
 export default function GrandHotelDashBoard() {
   const [isNavigationOpen, setIsNavigationOpen] = useState(false)
@@ -20,6 +21,20 @@ export default function GrandHotelDashBoard() {
 
         <Navigation isOpen={isNavigationOpen} />
       </div>
+
+      {/* header */}
+      <header className='hotel__header'>
+        <div className='hotel__header-brand'>
+          <img src={crown} width={80} />
+          <h3 className='hotel__header-heading'>Grand Hotel</h3>
+        </div>
+
+        <div className='hotel__banner'>
+          <h1 className='hotel__banner-heading'>Welcome to Grand Hotel</h1>
+          <p className='hotel__banner-paragraph'>Make your life luxurious</p>
+          <button className='hotel__banner-btn'>Check Out</button>
+        </div>
+      </header>
     </div>
   )
 }
